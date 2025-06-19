b919e79bd7a6afda17113cb82573abe6bf3f15ba
tfeWilliam <soulayman.william@gmail.com>
2025-06-19 22:31:14 +0200

    Update csrf_service.dart

--- lib/services/providers/csrf_service.dart
+++ lib/services/providers/csrf_service.dart
@@ -55,28 +55,3 @@ class CSRFService {
     }
   }
 }
-
-
-
-
-
-
-// import 'package:http/http.dart' as http;
-//
-// class CSRFService {
-//   static String? csrfToken;
-//
-//   static Future<void> fetchCSRFToken() async {
-//     final response = await http.get(Uri.parse('https://www.hairbnb.site/api/services/'));
-//     if (response.headers.containsKey('set-cookie')) {
-//       final cookies = response.headers['set-cookie'];
-//       if (cookies != null) {
-//         final match = RegExp(r'csrftoken=([^;]+)').firstMatch(cookies);
-//         if (match != null) {
-//           csrfToken = match.group(1);
-//           print('CSRF Token fetched: $csrfToken');
-//         }
-//       }
-//     }
-//   }
-// }
